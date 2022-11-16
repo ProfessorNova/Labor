b2b24f54999c402e0ad17d89ecdae33252928746
ProfessorNova <114916947+ProfessorNova@users.noreply.github.com>
2022-11-16 19:17:07 +0100

    Add files via upload

---
+++ Labor5/Labor5_Aufgabe12.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+    //deklariere Array
+    char arr[40];
+
+    //lese String ein
+    scanf("%s", arr);
+
+    //gebe string aus
+    printf("%s", arr);
+
+    return 0;
+}
